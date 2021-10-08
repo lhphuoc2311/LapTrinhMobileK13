ad5f54018ef1c8e90c840b0560c86a76bf57a0b9
Code Guru Mouse <phuoc2311@gmail.com>
2021-10-08 07:22:59 +0700

    initial commit

---
+++ app/src/main/java/vn/edu/giadinh/myfirstandroidproject/Activity2.java
@@ -0,0 +1,14 @@
+package vn.edu.giadinh.myfirstandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
---
+++ app/src/main/java/vn/edu/giadinh/myfirstandroidproject/MainActivity.java
@@ -0,0 +1,40 @@
+package vn.edu.giadinh.myfirstandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
